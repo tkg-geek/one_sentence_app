9d3fbc2886c077678a1023a00f3d14af17ef2867
tkg-geek <tkg.geek@gmail.com>
2025-02-13 00:01:36 +0900

    adnroid build

--- android/app/src/main/java/jp/ne/tkgeek/one_sentence/MainActivity.java
+++ android/app/src/main/java/jp/ne/tkgeek/one_sentence/MainActivity.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowInsets;
 import android.webkit.WebView;
+import android.util.Log;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    private static final String TAG = "MainActivity";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,11 +17,19 @@ public class MainActivity extends BridgeActivity {
         View decorView = getWindow().getDecorView();
         decorView.setOnApplyWindowInsetsListener((v, insets) -> {
             int navigationBarHeight = insets.getSystemWindowInsetBottom();
+            Log.d(TAG, "Navigation bar height: " + navigationBarHeight + "px");
             
+            // 実際のデバイスのdpも確認
+            float density = getResources().getDisplayMetrics().density;
+            float heightInDp = navigationBarHeight / density;
+            Log.d(TAG, "Navigation bar height in dp: " + heightInDp + "dp");
+
             WebView webView = findViewById(com.getcapacitor.android.R.id.webview);
             webView.evaluateJavascript(
                 String.format(
+                    "console.log('Navigation bar height from Java: %dpx'); " +
                     "document.documentElement.style.setProperty('--nav-bar-height', '%dpx');",
+                    navigationBarHeight,
                     navigationBarHeight
                 ),
                 null
@@ -26,4 +37,4 @@ public class MainActivity extends BridgeActivity {
             return insets;
         });
     }
-}
+} 
