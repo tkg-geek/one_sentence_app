db2e90827332e6a8729908bb07e36d315b92c05d
tkg-geek <tkg.geek@gmail.com>
2025-02-12 17:35:39 +0900

    iOS回転修正

---
+++ www/assets/index-hijX2jpo.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-K9JiwuOS.js","assets/index-DNjmFL_i.js"])))=>i.map(i=>d[i]);
+import{_ as p}from"./index-VZHKDcIO.js";import{registerPlugin as r}from"./index-DNjmFL_i.js";const _=r("App",{web:()=>p(()=>import("./web-K9JiwuOS.js"),__vite__mapDeps([0,1])).then(e=>new e.AppWeb)});export{_ as App};
